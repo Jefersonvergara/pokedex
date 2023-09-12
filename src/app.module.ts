f39c42695c35ef2cf3cc86ff5fadfa616ec101f0
jeferson <jf.vergara@gmail.com>
2023-09-12 09:59:09 -0500

    init app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+
+/* @Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
+
+  controllers: [AppController],
+  providers: [AppService], 
+})
+export class AppModule {} */
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    PokemonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
